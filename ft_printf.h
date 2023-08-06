e0fc6065a01f8d098f30b5fe9db9106ce2e29dcf
zeynepatilgan <138469159+zeynepatilgan@users.noreply.github.com>
2023-08-06 10:21:11 +0300

    printf

---
+++ ft_printf.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zeatilga <zeatilga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/01 18:41:09 by zeatilga          #+#    #+#             */
+/*   Updated: 2023/08/01 18:41:09 by zeatilga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *str, ...);
+int	ft_putchar(char x);
+int	ft_format(va_list *args, char w);
+int	ft_int(int w);
+int	ft_unsigned(unsigned int w);
+int	ft_string(char *str);
+int	ft_hex(unsigned int d, char w);
+int	ft_point(unsigned long w, int i);
+int	ft_check(char str);
+#endif
